5e678d887f9dfcc477692ea1bdbc728e7864de4d
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-05-31 15:28:29 +0900

    would u duo

---
+++ src/main/java/com/example/woulduduo/WoulduduoApplication.java
@@ -0,0 +1,13 @@
+package com.example.woulduduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoulduduoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoulduduoApplication.class, args);
+	}
+
+}
