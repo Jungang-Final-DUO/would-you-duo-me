72c7d5b0e294941f5e7fef5d54511dc86e29871c
jhlee <jhlee9462@github.com>
2023-06-13 14:13:41 +0900

    initial config

--- src/main/java/site/woulduduo/WoulduduoApplication.java
+++ src/main/java/site/woulduduo/WoulduduoApplication.java
@@ -1,4 +1,4 @@
-package com.example.woulduduo;
+package site.woulduduo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/site/woulduduo/WoulduduoApplicationTests.java
+++ src/test/java/site/woulduduo/WoulduduoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.woulduduo;
+package site.woulduduo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
