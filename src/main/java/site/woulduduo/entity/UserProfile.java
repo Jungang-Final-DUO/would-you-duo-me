ca9a8f134cf62458ae11ed6c0ae4967326abf80c
jhlee <jhlee9462@github.com>
2023-06-13 18:05:02 +0900

    in progress - making UserProfile entity

---
+++ src/main/java/site/woulduduo/entity/UserProfile.java
@@ -0,0 +1,30 @@
+package site.woulduduo.entity;
+
+
+import lombok.*;
+import org.hibernate.annotations.Check;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@ToString(exclude = {})
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "profileNo")
+@Builder
+@Entity
+@Table(name = "duo_user_profile")
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long profileNo;
+
+    @Column(length = 1000, nullable = false)
+    private String profileImage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+
+}
