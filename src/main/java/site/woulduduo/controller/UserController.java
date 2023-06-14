9bf6ccf302fec7aa2d7c25cb5ea4daf8150eb84d
HongChaa <126743377+HongChaa@users.noreply.github.com>
2023-06-15 01:27:32 +0900

    프로필 카드 등록 백단 프론트단 완성

--- src/main/java/site/woulduduo/controller/UserController.java
+++ src/main/java/site/woulduduo/controller/UserController.java
@@ -8,8 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import site.woulduduo.dto.request.page.AdminSearchType;
+import site.woulduduo.dto.request.user.UserCommentRequestDTO;
 import site.woulduduo.dto.response.ListResponseDTO;
 import site.woulduduo.dto.response.user.UsersByAdminResponseDTO;
 import site.woulduduo.service.UserService;
@@ -23,10 +25,20 @@ public class UserController {
 
     private final UserService userService;
 
+    // 마이페이지 - 프로필 카드 등록페이지 열기
     @GetMapping("/user/register-duo")
     public String registerDUO(/*HttpSession session, */Model model) {
 
         return "my-page/mypage-duoprofile";
+    }
+    @PostMapping("/user/register-duo")
+    public String registerDUO(/*HttpSession session, */UserCommentRequestDTO dto) {
+
+        boolean b = userService.registerDUO(/*session, */dto);
+        log.info("프로필카드등록 성공여부 : {}", b);
+        log.info("@@@@dto@@@@ :{}", dto);
+
+        return "redirect:/user/register-duo";
     }
 
     @GetMapping("/user/admin")
