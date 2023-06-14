09858c7eac72802985e67f492dd9dfd617b69e8f
HongChaa <126743377+HongChaa@users.noreply.github.com>
2023-06-14 17:17:41 +0900

    마이페이지 프로필카드 등록 백단 완성

---
+++ src/main/java/site/woulduduo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package site.woulduduo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.woulduduo.entity.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
---
+++ src/main/java/site/woulduduo/service/UserService.java
@@ -0,0 +1,55 @@
+package site.woulduduo.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.woulduduo.dto.request.user.UserCommentRequestDTO;
+import site.woulduduo.entity.User;
+import site.woulduduo.enumeration.Gender;
+import site.woulduduo.enumeration.Tier;
+import site.woulduduo.repository.UserRepository;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+
+    final String id = "abc1234";
+    public boolean registerDUO(/*HttpSession session, */UserCommentRequestDTO dto) {
+
+        User exUser = User.builder()
+                .userSessionId("abc1234@ddd.com")
+                .userAccount("abc1234@ddd.com")
+                .userPassword("12345678")
+                .lolTier(Tier.CHA)
+                .userGender(Gender.M)
+                .userBirthday(LocalDate.of(2000, 03, 16))
+                .userNickname("HongChaa")
+                .lolNickname("HongChaa")
+                .build();
+
+        userRepository.save(exUser);
+
+        Optional<User> user = userRepository.findById(exUser.getUserSessionId());
+
+        System.out.println("user = " + user);
+        if(user.isEmpty()) {
+            return false;
+        }
+        user.ifPresent(u -> {
+            u.setUserPosition(dto.getUserPosition());
+            u.setUserComment(dto.getUserComment());
+            u.setUserMatchingPoint(dto.getUserMatchingPoint());
+
+            userRepository.save(u);
+        });
+        return true;
+    }
+}
