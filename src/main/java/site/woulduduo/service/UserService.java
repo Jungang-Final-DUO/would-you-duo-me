0dd327c6cca47019235e2d3a7d3b8de1b9fb63c1
myblog <myblog0419@naver.com>
2023-06-14 12:07:30 +0900

    back 시작

---
+++ src/main/java/site/woulduduo/service/UserService.java
@@ -0,0 +1,27 @@
+//package site.woulduduo.service;
+//
+//public class UserService {
+//
+//    public ListResponseDTO<UsersByAdminResponseDTO> getUserListByAdmin(PageDTO dto,AdminViewType type){
+//
+//        return null;
+//    }
+//
+//    public UserDetailByAdminResponseDTO getUserDetailByAdmin(String userAccount){
+//
+//        return null;
+//    }
+//
+//    public boolean increaseUserPoint(UserModifyRequestDTO dto){
+//
+//        return false;
+//    }
+//
+//    public boolean changeUserPoint(UserModifyRequestDTO dto){
+//
+//        return false;
+//    }
+//
+//
+//
+//}
