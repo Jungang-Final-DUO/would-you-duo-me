3154aeee15d9591820a5d44deb8fa266eb871011
jhlee <jhlee9462@github.com>
2023-06-15 14:24:37 +0900

    complete - close modal

--- src/main/resources/static/assets/js/common/header.js
+++ src/main/resources/static/assets/js/common/header.js
@@ -1,7 +1,8 @@
-import {addModalBtnEvent} from "./modal-handler.js";
+import {addModalBtnEvent, addModalCloseEvent} from "./modal-handler.js";
 import {toBack} from "../chatting/chatting-modal.js";
 
 (() => {
     addModalBtnEvent();
+    addModalCloseEvent();
     toBack();
 })();
--- src/main/resources/static/assets/js/common/modal-handler.js
+++ src/main/resources/static/assets/js/common/modal-handler.js
@@ -5,8 +5,11 @@ function modalHandler($dropDownBtn) {
     $dropDownBtn.onclick = e => {
         e.preventDefault();
         const $target = e.target.closest('.modal-btn').nextElementSibling;
-        $target.showModal();
 
+        if (!$target.hasAttribute('open'))
+            $target.showModal();
+        else
+            $target.close();
     }
 }
 
@@ -15,5 +18,16 @@ export function addModalBtnEvent() {
     [...document.querySelectorAll('.modal-btn')].forEach(
         $modalBtn => modalHandler($modalBtn)
     );
+
+}
+
+export function addModalCloseEvent() {
+
+    [...document.querySelectorAll('dialog')].forEach(
+        $dialog => $dialog.onclick = e => {
+            if (e.target.matches('dialog'))
+                $dialog.close();
+        }
+    )
 
 }
