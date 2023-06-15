8d8d1d8c383692ffb46dcfa38611ac41b9c77e55
jhlee <jhlee9462@github.com>
2023-06-15 16:01:35 +0900

    complete - add to top btn event handler

--- src/main/resources/static/assets/js/common/header.js
+++ src/main/resources/static/assets/js/common/header.js
@@ -1,8 +1,15 @@
 import {addModalBtnEvent, addModalCloseEvent} from "./modal-handler.js";
 import {toBack} from "../chatting/chatting-modal.js";
+import {toTopBtnHandler} from "./to-top-btn-handler.js";
 
 (() => {
+    // 모달 열고 닫는 이벤트
     addModalBtnEvent();
     addModalCloseEvent();
+
+    // 채팅 메세지 모달 안에서 채팅방 모달로 돌아가기
     toBack();
+
+    // 위로가기 버튼 이벤트
+    toTopBtnHandler();
 })();
