16eb0e6489148198efd370cf6f0c6028dfd78ad5
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-06-18 20:47:11 +0900

    matching request, confirm, reject completed

--- src/main/java/site/woulduduo/repository/MatchingRepository.java
+++ src/main/java/site/woulduduo/repository/MatchingRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import site.woulduduo.entity.Matching;
 
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
+    Matching findByMatchingNo(long matchingNo);
 }
