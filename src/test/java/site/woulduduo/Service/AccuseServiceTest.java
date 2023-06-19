457e4ec8ac36bcf066a25dcab974eac2a94250e3
myblog <myblog0419@naver.com>
2023-06-19 20:55:37 +0900

    accuseuserBack 테스트 완료

--- src/test/java/site/woulduduo/Service/AccuseServiceTest.java
+++ src/test/java/site/woulduduo/Service/AccuseServiceTest.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import site.woulduduo.dto.request.accuse.UserAccuseRequestDTO;
 import site.woulduduo.dto.response.accuse.AccuseListResponseDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -36,6 +38,26 @@ class AccuseServiceTest {
         System.out.println("accuseByAdminResponseDTOS = " + accuseByAdminResponseDTOS);
         int size = accuseByAdminResponseDTOS.size();
         System.out.println("size = " + size);
+    }
+
+    @Test
+    @DisplayName("관리자용 금일 accuseList 확인")
+    void accuseUser(){
+
+        List<String>accuseType=new ArrayList<>();
+        accuseType.add("1번경고");
+        accuseType.add("2번경고");
+        accuseType.add("3번경고");
+
+
+        UserAccuseRequestDTO accuse = UserAccuseRequestDTO.builder()
+                .userAccount("123")
+                .accuseType(accuseType)
+                .accuseEtc("기타")
+                .build();
+
+        boolean booleanResult = accuseService.accuseUser(accuse);
+
     }
 
 }
