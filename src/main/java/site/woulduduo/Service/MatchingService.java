85060739362e5567fbce72fa69e25a83947e70c5
jhlee <jhlee9462@github.com>
2023-06-20 16:35:07 +0900

    complete - get gotten review list

--- src/main/java/site/woulduduo/Service/MatchingService.java
+++ src/main/java/site/woulduduo/Service/MatchingService.java
@@ -2,16 +2,25 @@ package site.woulduduo.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.woulduduo.dto.request.chatting.MatchingFixRequestDTO;
 import site.woulduduo.dto.request.chatting.MatchingMakeRequestDTO;
 import site.woulduduo.dto.request.chatting.ReviewWriteRequestDTO;
+import site.woulduduo.dto.response.ListResponseDTO;
+import site.woulduduo.dto.response.page.PageResponseDTO;
+import site.woulduduo.dto.response.user.UserReviewResponseDTO;
 import site.woulduduo.entity.Chatting;
 import site.woulduduo.entity.Matching;
 import site.woulduduo.enumeration.MatchingStatus;
 import site.woulduduo.repository.ChattingRepository;
 import site.woulduduo.repository.MatchingRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -65,9 +74,9 @@ public class MatchingService {
 
     /**
      * 리뷰 작성하기
+     *
      * @param userAccount - 리뷰를 쓰는 사람의 userAccount
-     * @param dto - 리뷰 작성에 필요한 dto
-     * @return - 리뷰 작성에 성공했는지 실패했는지 리턴
+     * @param dto         - 리뷰 작성에 필요한 dto
      */
     public void writeReview(final String userAccount, final ReviewWriteRequestDTO dto) {
 
@@ -83,4 +92,35 @@ public class MatchingService {
 
         matchingRepository.save(foundMatching);
     }
+
+    /**
+     * 해당 유저가 받은 모든 리뷰를 리턴
+     * @param userAccount - 해당 유저의 계정 명
+     * @param pageNo - 페이지 번호
+     * @return - 리뷰 목록
+     */
+    public ListResponseDTO<UserReviewResponseDTO, Matching> getGottenReview(
+           final String userAccount, final int pageNo
+    ) {
+
+        PageRequest pageInfo = PageRequest.of(pageNo - 1,
+                10,
+                Sort.by("matchingReviewRate").descending()
+        );
+
+        Page<Matching> pageDTO = matchingRepository
+                .findOneByChattingTo(userAccount, pageInfo);
+
+        List<UserReviewResponseDTO> hasReviewMatchingList = pageDTO
+                .getContent().stream()
+                .map(UserReviewResponseDTO::new)
+                .collect(Collectors.toList());
+
+        return ListResponseDTO.<UserReviewResponseDTO, Matching>builder()
+                .count(pageNo)
+                .pageInfo(new PageResponseDTO<>(pageDTO))
+                .list(hasReviewMatchingList)
+                .build();
+    }
+
 }
--- src/main/java/site/woulduduo/api/MatchingController.java
+++ src/main/java/site/woulduduo/api/MatchingController.java
@@ -5,19 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import site.woulduduo.dto.request.chatting.ReviewWriteRequestDTO;
+import site.woulduduo.dto.response.ListResponseDTO;
+import site.woulduduo.dto.response.user.UserReviewResponseDTO;
+import site.woulduduo.entity.Matching;
 import site.woulduduo.service.MatchingService;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/matchings")
 @RequiredArgsConstructor
 @Slf4j
 public class MatchingController {
 
     private final MatchingService matchingService;
 
-    @RequestMapping(value = "/api/v1/matchings/reviews", method = {RequestMethod.PATCH, RequestMethod.PUT})
+    @RequestMapping(value = "/reviews", method = {RequestMethod.PATCH, RequestMethod.PUT})
     public ResponseEntity<?> writeReview(HttpSession session, @RequestBody ReviewWriteRequestDTO dto) {
 
 //       세션에 로그인처리가 완료된 이후에 사용
@@ -36,5 +39,25 @@ public class MatchingController {
 
     }
 
+    /**
+     * 유저가 쓴 리뷰를 가져오는 메서드
+     * @param userAccount - 해당 유저
+     * @return - 유저가 쓴 리뷰 목록을 담은 응답
+     */
+    @GetMapping("/reviews/{userAccount}/{pageNo}")
+    public ResponseEntity<?> getWrittenReview(
+            @PathVariable String userAccount,
+            @PathVariable int pageNo
+            ) {
+
+        ListResponseDTO<UserReviewResponseDTO, Matching> responseDTO = null;
+        try {
+            responseDTO = matchingService.getGottenReview(userAccount, pageNo);
+        } catch (RuntimeException e) {
+            ResponseEntity.internalServerError().body("알 수 없는 에러가 발생했습니다");
+        }
+
+        return ResponseEntity.ok(responseDTO);
+    }
 
 }
