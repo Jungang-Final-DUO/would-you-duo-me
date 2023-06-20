71bad3899b6a9ab3e9d91d2a2f70df19446d4b6e
jhlee <jhlee9462@github.com>
2023-06-20 14:51:00 +0900

    complete - writeReview method

--- src/main/java/site/woulduduo/api/MatchingController.java
+++ src/main/java/site/woulduduo/api/MatchingController.java
@@ -3,10 +3,8 @@ package site.woulduduo.api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import site.woulduduo.dto.request.chatting.ReviewWriteRequestDTO;
 import site.woulduduo.service.MatchingService;
 
 import javax.servlet.http.HttpSession;
@@ -19,9 +17,24 @@ public class MatchingController {
 
     private final MatchingService matchingService;
 
-    @PostMapping("/chattings/{userAccount}")
-    public ResponseEntity<?> makeChatting(HttpSession session, @PathVariable String userAccount) {
+    @RequestMapping(value = "/api/v1/matchings/reviews", method = {RequestMethod.PATCH, RequestMethod.PUT})
+    public ResponseEntity<?> writeReview(HttpSession session, @RequestBody ReviewWriteRequestDTO dto) {
+
+//       세션에 로그인처리가 완료된 이후에 사용
+//       String userAccount = session.getAttribute(LOGIN_KEY);
+
+        String userAccount = "user1";
+
+        try {
+            matchingService.writeReview(userAccount, dto);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest()
+                    .body(e.getMessage());
+        }
+
         return ResponseEntity.ok().build();
+
     }
+
 
 }
