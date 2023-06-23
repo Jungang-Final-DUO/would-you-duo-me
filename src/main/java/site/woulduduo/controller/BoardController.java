5a2311967fd5e753252688a9e69a3c0dc4158efc
myblog <myblog0419@naver.com>
2023-06-23 15:49:47 +0900

    BoardController 리퀘스트 매핑 주석

--- src/main/java/site/woulduduo/controller/BoardController.java
+++ src/main/java/site/woulduduo/controller/BoardController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.woulduduo.dto.request.board.BoardModifyRequestDTO;
 import site.woulduduo.dto.request.board.BoardWriteRequestDTO;
@@ -20,7 +19,7 @@ import javax.servlet.http.HttpSession;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/board") //경로
+//@RequestMapping("/api/v1/board") //경로
 
 public class BoardController {
 
