5e3331b1aff2b3a04b950396125db17df330fd70
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-06-23 10:25:58 +0900

    matching - in progress

--- src/main/resources/static/assets/js/chatting/matching.js
+++ src/main/resources/static/assets/js/chatting/matching.js
@@ -1,12 +1,40 @@
-import {outputMessage, renderAndSaveMessage, scrollDown} from "./messageRendering.js";
+import {renderAndSaveMessage, scrollDown} from "./messageRendering.js";
 
+// 매칭요청을 받은 사람의 이벤트
 export function matchingRequestEvent(){
     const requestBtn = [...document.querySelectorAll('.matching-request')];
     requestBtn.forEach(
         mr => mr.onclick = e => {
-       const $chattingNo = e.target.closest('.chatting-card').id;
-       matchingRequest($chattingNo);
-    });
+            const $chattingNo = e.target.closest('.chatting-card').id;
+            matchingRequest($chattingNo);
+        });
+}
+
+// 요청을 받은 사람의 이벤트
+export function matchingResponseEvent(){
+    const requestedBtn = [...document.querySelectorAll('.matching-requested')];
+    requestedBtn.forEach(
+        mr => mr.onclick = e => {
+            const $chattingNo = e.target.closest('.chatting-card').id;
+            matchingConfirm($chattingNo);
+        });
+}
+
+function  matchingConfirm(chattingNo) {
+    const requestInfo = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: chattingNo
+    }
+
+    fetch(`/api/v1/matchings`, requestInfo)
+        .then(res => res.json())
+        .then(result => {
+            changeMatchingStatus(chattingNo, result);
+            sendNoticeMessage(chattingNo, '매칭이 신청되었습니다.');
+        });
 }
 
 function matchingRequest(chattingNo){
@@ -33,6 +61,7 @@ function changeMatchingStatus(chattingNo, result) {
     const chatCard = document.getElementById(chattingNo);
     const $target = chatCard.querySelector('.matching-accept-btn');
     $target.classList.remove('matching-request');
+    $target.setAttribute('disabled', 'disabled');
     $target.classList.add('matching-requested');
     $target.dataset.matchingNo = result;
     $target.childNodes[1].nodeValue = '수락대기중';
