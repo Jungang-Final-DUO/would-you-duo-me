c0e40c5ef6c574570d70f1aca18b190834e2ab92
jhlee <jhlee9462@github.com>
2023-06-30 09:39:17 +0900

    complete - update tier and most champ info when sign in

--- src/main/java/site/woulduduo/repository/MostChampRepository.java
+++ src/main/java/site/woulduduo/repository/MostChampRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import site.woulduduo.entity.MostChamp;
 
 public interface MostChampRepository extends JpaRepository<MostChamp, Long> {
+
+    MostChamp getAllByUser_UserAccountAndMostNo(String userAccount, int mostNo);
+
 }
