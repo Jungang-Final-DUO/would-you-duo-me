46cfd1153b8bdcd5eaf2c11a4629301bbc220573
jhlee <jhlee9462@github.com>
2023-06-30 16:37:49 +0900

    complete - bug fix in index page

--- src/main/java/site/woulduduo/repository/UserQueryDSLRepositoryCustom.java
+++ src/main/java/site/woulduduo/repository/UserQueryDSLRepositoryCustom.java
@@ -4,12 +4,11 @@ import site.woulduduo.dto.request.page.UserSearchType;
 import site.woulduduo.dto.request.user.UserCommentRequestDTO;
 import site.woulduduo.dto.response.user.UserProfileResponseDTO;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 public interface UserQueryDSLRepositoryCustom {
 
-    List<UserProfileResponseDTO> getUserProfileList(UserSearchType userSearchType/*, HttpSession session*/);
+    List<UserProfileResponseDTO> getUserProfileList(UserSearchType userSearchType);
 
-    int modifyProfileCard(UserCommentRequestDTO dto/* , HttpSession session*/);
+    int modifyProfileCard(UserCommentRequestDTO dto);
 }
