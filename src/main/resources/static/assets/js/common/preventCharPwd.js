4c84b6d73ccbacb2ce7d0f8839e560a5752b8a17
jhlee <jhlee9462@github.com>
2023-07-01 15:34:12 +0900

    complete - can type !@#$%^&*_?~ in password

--- src/main/resources/static/assets/js/common/preventCharPwd.js
+++ src/main/resources/static/assets/js/common/preventCharPwd.js
@@ -7,8 +7,8 @@ function chkCharCode(event) {
         (keyCode === 32) || // Space
         (keyCode === 8) || // BackSpace
         (keyCode === 189) || // Dash
-        (keyCode === 161 && keyCode === 64 && keyCode === 163 && keyCode === 164 && keyCode === 165 && keyCode === 166
-            && keyCode === 167 && keyCode === 160 && keyCode === 170 && keyCode === 63 && keyCode === 176)
+        (keyCode === 161 || keyCode === 64 || keyCode === 163 || keyCode === 164 || keyCode === 165 || keyCode === 166
+            || keyCode === 167 || keyCode === 160 || keyCode === 170 || keyCode === 63 || keyCode === 176)
     );
     if (!isValidKey) {
         event.target.value = '';
