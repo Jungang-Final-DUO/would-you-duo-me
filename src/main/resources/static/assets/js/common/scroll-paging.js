ce8e40f779f7db432e17079cfdc5c76ed1c2b5f1
jhlee9462 <jhlee9462@gmail.com>
2023-07-03 00:40:13 +0900

    complete - bugfix scroll paging

--- src/main/resources/static/assets/js/common/scroll-paging.js
+++ src/main/resources/static/assets/js/common/scroll-paging.js
@@ -2,8 +2,6 @@ export function scrollPaging($wrapper, renderingCallback, callbackParam) {
 
     $wrapper.addEventListener('scroll', e => {
 
-        console.log('scrollTop:' + $wrapper.scrollTop + ', height:' + $wrapper.offsetHeight + ', scrollHeight:' + $wrapper.scrollHeight + ', clientHeight:');
-
         if ($wrapper.scrollTop + $wrapper.offsetHeight >= $wrapper.scrollHeight - 1) {
             renderingCallback(callbackParam);
         }
