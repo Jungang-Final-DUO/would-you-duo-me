1b97f988e357827c01fb4df96b1cc954079ac8a8
jhlee <jhlee9462@github.com>
2023-08-05 17:33:40 +0900

    complete - refresh user history info back

--- src/main/java/site/woulduduo/repository/RecentMatchRepository.java
+++ src/main/java/site/woulduduo/repository/RecentMatchRepository.java
@@ -1,7 +1,12 @@
 package site.woulduduo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import site.woulduduo.entity.RecentMatch;
 
 public interface RecentMatchRepository extends JpaRepository<RecentMatch, Long> {
+
+    @Query("delete from RecentMatch r where r.user.userAccount = :UserAccount")
+    void deleteAllByUserAccount(String UserAccount);
+
 }
